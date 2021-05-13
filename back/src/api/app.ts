5fe3fb5371a9d257619949f1761e7b450a178d0b
Andres Suarez <andres27059934@gmail.com>
2021-05-13 12:12:04 -0300

    Fix cors

--- back/src/api/app.ts
+++ back/src/api/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from 'cors'
 import { graphqlHTTP } from "express-graphql"
 import schema from "../services/graphql/schema"
 
 const app = express()
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
